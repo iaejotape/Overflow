1181983639becceebad63e9b26a4db97d127070f
Isac B.M <isacbritomatos32@gmail.com>
2025-05-15 11:25:38 -0300

    Iniciando a Branch

---
+++ src/pages/tela-inicial.jsx
@@ -0,0 +1,10 @@
+import Layout from '../componentes/layout';
+
+export default function PaginaInicial() {
+  return (
+    // O que tiver aqui dentro já tem uma div pai chamada ".principal"
+    <Layout> 
+      <h1>Bem-vindo à Plataforma Overflow!</h1>
+    </Layout>
+  );
+}
