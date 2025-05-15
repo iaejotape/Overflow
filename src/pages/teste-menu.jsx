7b18bea150d9244fdf9e5cf1c8012b9186a6f043
Isac B.M <isacbritomatos32@gmail.com>
2025-05-15 09:01:15 -0300

    Resolvendo os Bugs do CSS em outras páginas

--- src/pages/teste-menu.jsx
+++ src/pages/teste-menu.jsx
@@ -5,7 +5,6 @@ export default function TestePagina() {
     // O que tiver aqui dentro já tem uma div pai chamada ".principal"
     <Layout> 
       <h1>Bem-vindo à Plataforma Overflow!</h1>
-      <p>Isso aqui vai dentro da div .principal</p>
     </Layout>
   );
 }
