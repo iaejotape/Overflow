c3d45b2b2eb60c03b4cbc00d7d3e4c7534f61f22
DiogoBramorim <diogobamorim06@gmail.comgit config --global user.email diogobamorim06@gmail.com>
2025-05-25 17:33:47 -0300

    Telas rank

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import Editor from "./pages/editor-codigo.jsx";
 import QuestoesEvento from "./pages/questoes-evento.jsx";
 import MeusEventos from "./pages/tela-meus-eventos.jsx";
 import Inicio from "./pages/tela-inicial.jsx";
+import Rank_evento from "./pages/rank_evento.jsx";
+import Rank_geral from "./pages/rank_geral.jsx";
 
 function App() {
   return (
@@ -32,6 +34,8 @@ function App() {
            <Route path="/meus_eventos" element={<MeusEventos />}/>
             <Route path="/testebarra" element={<Barra />}/>
             <Route path="/inicio" element={<Inicio />}/>
+            <Router path="/rank_geral" element={<Rank_geral/>}/>
+            <Router path="/rank_evento" element={<Rank_evento/>}/>
           </Routes>
         </BrowserRouter>
       </div>
